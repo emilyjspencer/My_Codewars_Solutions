3576f61eb6a4c38b5337b59f96d301c78c921896
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-10 14:48:05 +0000

    Add hash to array

---
+++ Ruby/8kyu/add_hash_to_array.rb
@@ -0,0 +1,14 @@
+# Push a hash/an object into array
+# You are trying to put a hash in ruby or an object in javascript or java 
+# into an array, but it always returns error, solve it and keep it as simple as possible!
+
+items = Array.new
+items << {:a => "b", :c => "d"}
+
+
+
+
+# Could also do: 
+
+# items = []
+#items.push({{:a => "b", :c => "d"})
