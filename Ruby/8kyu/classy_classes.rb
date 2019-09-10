ec404703fa7104e9944aceaea734c1281a3fc9e4
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-10 18:14:05 +0000

    Classy Classes

---
+++ Ruby/8kyu/classy_classes.rb
@@ -0,0 +1,26 @@
+# Classy Classes
+
+# Basic Classes, this kata is mainly aimed at the new JS ES6 Update
+# introducing classes
+# Task
+# Your task is to complete this Class, the Person class has been created. 
+# You must fill in the Constructor method to accept a name as string and an 
+# age as number, complete the get Info property and getInfo method/Info getter 
+# which should return
+# johns age is 34
+
+
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  
+  def info
+    return "#{@name}s age is #{@age}"
+  end 
+  
+end
+
+man = Person.new("john", 34)
+ 
