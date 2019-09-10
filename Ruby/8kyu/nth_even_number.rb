ecc5d62d98fac955c2b0a1a02bfcfe94f9175325
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-10 14:12:08 +0000

    Nth even number

---
+++ Ruby/8kyu/nth_even_number.rb
@@ -0,0 +1,19 @@
+# Nth even number
+
+# Return the Nth Even Number
+
+# nthEven(1) //=> 0, the first even number is 0
+# nthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+
+# nthEven(100) //=> 198
+# nthEven(1298734) //=> 2597466
+
+def nth_even(n)
+  # return the nth even number
+  # numbers start from 0
+
+  return 2 * (n - 1)
+  
+end
+
+puts nth_even(10)
