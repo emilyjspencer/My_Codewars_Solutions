b6d001c21d2623db92312f38d4adba40c1ca9797
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-10 09:31:33 +0000

    Opposite number

---
+++ Ruby/8kyu/opposite_number.rb
@@ -0,0 +1,20 @@
+# Opposite number
+
+# Very simple, given a number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+
+
+
+def opposite(num)
+ if num < 0
+   return num * -1
+   
+ else
+   return num * -1
+ end
+end
