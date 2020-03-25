d5f8cf6b7d9f75079661f8554994d84777e6df29
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-03-25 13:25:14 +0000

    Removing every second element from array

---
+++ Python/8kyu/removing_elements.py
@@ -0,0 +1,9 @@
+# Take an array and remove every second element out of that array. 
+# Always keep the first element and start removing with the next element.
+
+my_list = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...]
+
+
+def remove_every_other(my_list):
+ 
+     return my_list[::2]
