052a9dedf2adace013838479368f063574dcf726
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-19 16:13:25 +0100

    Is it even?

---
+++ Ruby/8kyu/is_it_even?.rb
@@ -0,0 +1,17 @@
+# Is it even?
+
+# In this Kata we are passing a number (n) into a function.
+
+# Your code will determine if the number passed is even (or not).
+
+# The function needs to return either a true or false.
+
+# Numbers may be positive or negative, integers or floats.
+
+def test_even(n)
+  if n % 2 == 0
+    return true
+  else
+    return false
+  end
+end
