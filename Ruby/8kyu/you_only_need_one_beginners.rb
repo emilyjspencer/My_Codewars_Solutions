3c49329b8ce67c61c88c95c7ec76f1451581e22d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-19 14:25:16 +0100

    You only need one - beginner

---
+++ Ruby/8kyu/you_only_need_one_beginners.rb
@@ -0,0 +1,15 @@
+# You only need one - beginner
+# You will be given an array a and a value x. 
+# All you need to do is check whether the provided array contains the value.
+
+# Array can contain numbers or strings. X can be either.
+
+# Return true if the array contains the value, false if not.
+
+def check(arr,element)
+ if arr.include? element
+   return true
+ else
+   return false
+ end
+end
