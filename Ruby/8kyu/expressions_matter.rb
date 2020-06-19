fe9d6a46b6db8f63e9f142c79c6e06507eb2836f
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-19 14:38:03 +0100

    Expressions Matter

---
+++ Ruby/8kyu/expressions_matter.rb
@@ -0,0 +1,10 @@
+# Expressions matter
+
+# Given three integers a ,b ,c, return the largest number obtained after inserting the following operators
+#  and brackets: +, *, ()
+# In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+def expression_matter(a,b,c)
+  array = [(a + b + c), ((a * b) + c), (a + (b * c)), (a * b * c), (a * (b + c)), ((a + b)* c)]
+  array.max
+end
